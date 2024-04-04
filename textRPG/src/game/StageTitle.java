85595c665f2483894805ab5db6c2de639b0400a3
dayeon <da092511@naver.com>
2024-04-04 14:08:11 +0900

    Modify : update method

--- textRPG/src/game/StageTitle.java
+++ textRPG/src/game/StageTitle.java
@@ -1,13 +1,23 @@
 package game;
 
+import java.util.Scanner;
+
 public class StageTitle extends Stage{
-	
 	@Override
 	public void init() {
 	}
 	
 	@Override
 	public boolean update() {
+		System.out.println("[시작] [종료]");
+		System.out.print(": ");
+		String answer = GameManager.scan.next();
+		
+		if(answer.equals("시작"))
+			GameManager.nextStage = "StageLobby";
+		else if(answer.equals("종료"))
+			GameManager.nextStage = "";
+		
 		return false;
 	}
 }
