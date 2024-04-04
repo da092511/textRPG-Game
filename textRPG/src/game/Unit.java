8037d2407e49bed6c0bb9a6f6ca53663c26801d4
dayeon <da092511@naver.com>
2024-04-04 11:50:05 +0900

    Add : attack method

--- textRPG/src/game/Unit.java
+++ textRPG/src/game/Unit.java
@@ -1,5 +1,21 @@
 package game;
 
 public class Unit {
-
+	
+	private int maxHp;
+	private int curHp;
+	private int power;
+	
+	private boolean isDead;
+	
+	private boolean isFaint;
+	private int faintTurn ;
+	
+	
+	
+	
+	public void attack(Unit target) {
+		
+	}
+	
 }
