4f9cd6fa8bcc27d4f57e537b26079dc712bb093e
dayeon <da092511@naver.com>
2024-04-04 12:10:10 +0900

    Add : getCode method

--- textRPG/src/game/Player.java
+++ textRPG/src/game/Player.java
@@ -1,5 +1,23 @@
 package game;
 
 public class Player extends Unit{
-
+	private int code;
+	
+	private int money;
+	
+	public static Inventory inven  = new Inventory();
+	public static Guild guild = new Guild();
+	
+	public Player() {
+		
+	}
+	
+	public Player(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return this.code;
+	}
+	
 }
