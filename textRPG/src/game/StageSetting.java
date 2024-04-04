36b14b7e273dcc3d90e61a5d1a5815d693b183b8
dayeon <da092511@naver.com>
2024-04-04 14:34:29 +0900

    Modify : update method

--- textRPG/src/game/StageSetting.java
+++ textRPG/src/game/StageSetting.java
@@ -8,6 +8,27 @@ public class StageSetting extends Stage{
 	
 	@Override
 	public boolean update() {
+		while(true) {
+			System.out.println("[1. 길드관리] [2. 상점] [3. 인벤토리]");
+			System.out.println("[4. 저장] [5. 로드] [0. 종료]");
+			
+			int option = inputNumber();
+			
+			if(option == 1)
+				Guild.run();
+			else if(option == 2) 
+				Shop.run();
+			else if(option == 3) {
+				Inventory.run();
+			}
+			else if(option == 4)
+				FileManager.save();
+			else if(option == 5)
+				FileManager.load();
+			else if(option == 0)
+				break;
+		}
+			
 		return false;
 	}
 	
