c9dbb755f2f7db97edd49d6b60d399c3ace00aef
dayeon <da092511@naver.com>
2024-04-04 15:09:52 +0900

    Add : clone method

--- textRPG/src/game/Item.java
+++ textRPG/src/game/Item.java
@@ -1,5 +1,28 @@
 package game;
 
 public class Item {
-
+	public static final int WEAPON = 1; //무기
+	public static final int ARMOR = 2; //갑옷
+	public static final int RING = 3; //반지
+	
+	private int kind;
+	private String name;
+	private int price;
+	private int power;
+	
+	public Item() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Item(int kind, String name, int price, int power) {
+		this.kind  = kind;
+		this.name = name;
+		this.price = price;
+		this.power = power;
+	}
+	
+	public Item clone() {
+		return new Item(this.kind, this.name, this.price, this.power);
+	}
+	
 }
