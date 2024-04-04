81986042ac0262d912dc2ef60ce5055a676ea5ff
dayeon <da092511@naver.com>
2024-04-04 14:33:51 +0900

    Add : run method

--- textRPG/src/game/Inventory.java
+++ textRPG/src/game/Inventory.java
@@ -1,5 +1,14 @@
 package game;
 
-public class Inventory {
+import java.util.Vector;
 
+public class Inventory {
+	Vector <Item> allItems = new Vector<>();
+	Vector<Item> wearingItems = new Vector<>();
+	
+	public static void run() {
+		
+	}
+	
+	
 }
