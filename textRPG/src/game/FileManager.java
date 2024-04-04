e24fa6d9e622501e2364541853580de82b30fad5
dayeon <da092511@naver.com>
2024-04-04 14:22:36 +0900

    Add : save method

---
+++ textRPG/src/game/FileManager.java
@@ -0,0 +1,23 @@
+package game;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager {
+	private String fileName = "TXTRPG";
+	private File file = new File(fileName);
+	
+	private FileWriter fw;
+	private FileReader fr;
+	private BufferedReader br;
+	
+	
+	public static void save() {
+		
+	}
+	
+	
+}
