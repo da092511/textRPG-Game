8aa4888ba311501843a0b5684f21f2d53a0e9d9d
dayeon <da092511@naver.com>
2024-04-04 11:14:35 +0900

    Rename

--- textRPG/src/game/UnitManager.java
+++ textRPG/src/game/UnitManager.java
@@ -3,8 +3,17 @@ package game;
 public class UnitManager {
 	public static UnitManager instance = new UnitManager();
 	
+	String path = "game.";
+	String[] monster = {"MonsterOrc", "MonsterWolf", "MonsterBat"};
+	
+	int hpLimit;
+	int powLimt;
+	
 	public static UnitManager getInstance () {
 		return instance;
 	}
+	
+	
+	
 	
 }
