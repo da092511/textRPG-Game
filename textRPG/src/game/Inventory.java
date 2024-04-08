9ec75e3a9423d93bf9beaf7e29e2a3c0b55097ab
dayeon <da092511@naver.com>
2024-04-08 17:34:30 +0900

    Add : method(takeAway,run)

--- textRPG/src/game/Inventory.java
+++ textRPG/src/game/Inventory.java
@@ -80,5 +80,67 @@ public class Inventory {
 			System.out.println("착용완료");
 		}
 	}
+
+	private void takeAway() {
+		int n = 1;
+		for(Item item : wearItems) {
+			System.out.println("======================================");
+			System.out.println(n+". \t"+item);
+			System.out.println("\t착용여부 : [" + (item.getWearing() ? "O" : "X" )+ "]");
+		}
+		
+		System.out.print("제거할 아이템 번호 : ");
+		int select = GameManager.inputNumber()-1;
+		
+		Item target = allItems.get(select);
+		
+		System.out.println(target);
+		
+		if(!target.getWearing())
+			return;
+		
+		int type = target.getKind();
+		
+		if(type == 1 && !weapon)
+			return;
+		else if(type == 1) {
+			weapon = !weapon;
+			target.setWearing();
+			wearItems.add(target);
+			System.out.println("제거완료");
+			return;
+		}
+		else if(type == 2 && !armor)
+			return;
+		else if(type == 2) {
+			armor = !armor;
+			target.setWearing();
+			wearItems.add(target);
+			System.out.println("제거완료");
+			return;
+		}
+		else if(type == 3 && !ring)
+			return;
+		else if(type == 3) {
+			ring = !ring;
+			target.setWearing();
+			wearItems.add(target);
+			System.out.println("제거완료");
+		}
+			
+	}
+	
+	public void run() {
+		while(true) {
+			printMenu();
+			int option = GameManager.inputNumber();
+			if(option == 1) {
+				wearItem();
+			}else if(option == 2) {
+				takeAway();
+			}else if(option == 0)
+				break;
+		}
+	}
 	
 }
