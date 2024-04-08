43f70d71bf3526b0cccaf769db40030b7ee066dc
dayeon <da092511@naver.com>
2024-04-08 17:32:48 +0900

    Add : printMenu method

--- textRPG/src/game/Inventory.java
+++ textRPG/src/game/Inventory.java
@@ -3,12 +3,21 @@ package game;
 import java.util.Vector;
 
 public class Inventory {
+	Vector <Item> wearItems = new Vector<>();
 	Vector <Item> allItems = new Vector<>();
-	Vector<Item> wearingItems = new Vector<>();
 	
-	public static void run() {
-		
+	private boolean ring ;
+	private boolean weapon;
+	private boolean armor;
+	
+	public void getItem(Item item) {
+		allItems.add(item);
 	}
 	
+	private void printMenu() {
+		System.out.println("======================================");
+		System.out.println("[1.아이템 착용]\t[2.아이템 빼기]");
+		System.out.println("[0.뒤로가기]");
+	}
 	
 }
