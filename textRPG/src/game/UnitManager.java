e61e4f42f80b07b703d380408d3a3ecf6e108822
dayeon <da092511@naver.com>
2024-04-08 18:33:26 +0900

    Modify : delect review text

--- textRPG/src/game/UnitManager.java
+++ textRPG/src/game/UnitManager.java
@@ -16,7 +16,7 @@ public class UnitManager {
 	private int maxMonster;
 	
 	String path = "game.";
-	String[] monster = {"MonsterOrc", "MonsterWolf", "MonsterBat"};
+	String[] monster = {"MOrc", "MWolf", "MBat"};
 	
 	int hpLimit;
 	int powLimt;
@@ -27,26 +27,39 @@ public class UnitManager {
 	
 	public void setMonster(int size) {
 		for(int i=0;i<size;i++) {
-			int rIdx = ran.nextInt(3);
-			try {
-			Class<?> clazz = Class.forName(path + monster[rIdx]);
-			Object obj = clazz.getConstructor();
-			Monster mon = (Monster) obj;
-			
-			int hp = ran.nextInt(5)+6;
-			hp = hp * 10;
-			int power = ran.nextInt(6)+1;
-			power = power *5;
-			
-			mon.init(hp,power);
-			monsterList.add(mon);
-			
-			}catch(Exception e) {
-				
-			}
+			addMonster();
 		}
 	}
 	
+	private void addMonster() {
+		int rIdx = ran.nextInt(3);
+		
+		String name = "";
+		
+		if(rIdx == 0)
+			name += "오크";
+		else if(rIdx == 1)
+			name += "늑대";
+		else if(rIdx == 2)
+			name += "박쥐";
+		
+		int hp = ran.nextInt(5)+6;
+		hp = hp * 10;
+		int power = ran.nextInt(6)+1;
+		power = power *5;
+		
+		try {
+			Class<?> clazz = Class.forName(path + monster[rIdx]);
+			Object obj = clazz.getConstructor().newInstance();
+			Monster mon = (Monster) obj;
+			
+			mon.init(name,hp,power);
+			monsterList.add(mon);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static UnitManager getInstance () {
 		return instance;
 	}
