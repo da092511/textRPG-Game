48c54fa7d10bb900e5e34305d49bc2deb6b8096e
dayeon <da092511@naver.com>
2024-04-08 17:33:24 +0900

    Add : printItem method

--- textRPG/src/game/Inventory.java
+++ textRPG/src/game/Inventory.java
@@ -19,5 +19,26 @@ public class Inventory {
 		System.out.println("[1.아이템 착용]\t[2.아이템 빼기]");
 		System.out.println("[0.뒤로가기]");
 	}
+	private void printItem(){
+		int n = 1;
+		for(Item item : allItems) {
+			System.out.println("======================================");
+			System.out.println(n+". \t"+item);
+			System.out.println("\t착용여부 : [" + (item.getWearing() ? "O" : "X" )+ "]");
+			
+			if(item.getWearing()) {
+				if(item.getKind() == 1) 
+					weapon = true;
+				else if(item.getKind() == 2)
+					armor = true;
+				else if(item.getKind() == 3)
+					ring = true;
+			}
+			
+			n++;
+		}
+	}
+	
+	
 	
 }
